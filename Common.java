10e69b4a8eeed612f23509ce3c7a760737738372
Dmitry <dmitry@kishkin.su>
2019-01-24 21:18:47 +0300

    Auto stash before merge of "common" and "origin/common"
    znzn

--- Common.java
+++ Common.java
@@ -6,13 +6,7 @@ class Common {
 	Common(int id){
 		this.id = id;
 	}
-
-У попа была собака, он её любил она съела кусок мяса он её убил и на камне написал
-У попа была собака
-}
-
 Class Kishkin{
 int a;
 int b;
-}
 
